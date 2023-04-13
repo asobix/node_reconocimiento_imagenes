ebe8bce7721f4f8e2a658b47cd8ee717bdec7421
Reinaldo Requena <reinaldo.r.anincubator@gmail.com>
2023-04-13 16:31:22 +0200

    cambios para subir archivo

--- App.js
+++ App.js
@@ -2,21 +2,25 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const http = require('http');
-const predictRouter = require('./routes/router')
+const predictRouter = require('./routes/predict');
 
 const app = express();
-
-app.use(bodyParser.urlencoded({extended: false}));
+// We added bodyParser so that we can access `body` in `req` later
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/hello', function(req, res) {
-    res.send('hello')
-})
-app.use('/router', predictRouter)
+app.get('/hello', function (req, res) {
+  res.send('hello');
+});
+app.use('/predict', predictRouter);
+app.get('/', function (req, res) {
+  return res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 const port = process.env.PORT || '8080';
-app.set('port', port)
+
+app.set('port', port);
 const server = http.createServer(app);
 server.listen(port);
-server.on('listening', ()=>{
-    console.log(`Listening on ${port}`);
+server.on('listening', () => {
+  console.log('Listening on ' + port);
 });
